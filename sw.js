0e0012b522fd2646ba5cdc0059ef965b5b659146
phummiphat582-debug <phummiphat582@gmail.com>
2025-12-26 18:35:23 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,20 @@
-self.addEventListener('install', () => {
+const CACHE_NAME = "stock-app-v2";
+
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', () => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
